f68c0a0495f455cd7aacb14586652222216e12b3
vishnu R <157986076+vishnu-R2005@users.noreply.github.com>
2026-06-18 19:29:58 +0530

    feat(employees): implement employee management module and APIs

--- employees/views.py
+++ employees/views.py
@@ -1,10 +1,34 @@
 from rest_framework import viewsets
 from .models import Employee
 from .serializers import EmployeeSerializer
-
+from rest_framework import generics
+from services.permissions import IsAdminUserRole
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
     
+class EmployeeCreateView(
+    generics.CreateAPIView
+):
+
+    queryset = Employee.objects.all()
+
+    serializer_class = EmployeeSerializer
+
+    permission_classes = [
+        IsAdminUserRole
+    ]
+
+class EmployeeManageView(
+    generics.RetrieveUpdateDestroyAPIView
+):
+
+    queryset = Employee.objects.all()
+
+    serializer_class = EmployeeSerializer
+
+    permission_classes = [
+        IsAdminUserRole
+    ]
